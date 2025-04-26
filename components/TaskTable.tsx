de6f267e4663b011207e5c18989606a088c3bcf5
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-04-26 18:33:37 +0530

    mobile table view added

--- components/TaskTable.tsx
+++ components/TaskTable.tsx
@@ -1,22 +1,19 @@
 "use client";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import StatusBadge from "./StatusBadge";
-import { Edit, Trash } from "lucide-react";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useTasks } from "@/context/TaskContext";
 import EditTaskDialog from "./EditTaskDialog";
-import { Priority } from "@/types/task";
 import DeleteTaskDialog from "./DeleteTaskDialog";
+import TaskTableRow from "./TaskTableRow";
 
 const TaskTable = () => {
-  const { tasks, setEdit, setDeleteId, updateTask } = useTasks();
+  const { tasks } = useTasks();
   return (
     <Table className="w-full table-fixed">
-      <TableHeader>
+      <TableHeader className="hidden md:table-header-group">
         <TableRow className="border-primary">
           <TableHead className="whitespace-nowrap w-16">SL.No</TableHead>
           <TableHead className="whitespace-nowrap w-32">Title</TableHead>
-          <TableHead className="w-full">Description</TableHead>
+          <TableHead className="w-32 md:w-full">Description</TableHead>
           <TableHead className="whitespace-nowrap w-32">Due Date</TableHead>
           <TableHead className="whitespace-nowrap w-32">Status</TableHead>
           <TableHead className="whitespace-nowrap w-32">Priority</TableHead>
@@ -25,38 +22,7 @@ const TaskTable = () => {
       </TableHeader>
       <TableBody>
         {tasks.map((task, index) => (
-          <TableRow key={task.id}>
-            <TableCell className="whitespace-nowrap">{index + 1}</TableCell>
-            <TableCell className="truncate">{task.title}</TableCell>
-            <TableCell className="truncate">{task.description}</TableCell>
-            <TableCell className="whitespace-nowrap">{task.dueDate}</TableCell>
-            <TableCell className="whitespace-nowrap">
-              <StatusBadge status={task.status} />
-            </TableCell>
-            <TableCell className="whitespace-nowrap">
-              <Select value={task.priority} onValueChange={(e: Priority) => updateTask({ ...task, priority: e })}>
-                <SelectTrigger className="border-black w-[110px]">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="High">High</SelectItem>
-                  <SelectItem value="Medium">Medium</SelectItem>
-                  <SelectItem value="Low">Low</SelectItem>
-                </SelectContent>
-              </Select>
-            </TableCell>
-            <TableCell className="flex justify-center gap-2 mt-2">
-              <Edit onClick={() => setEdit(task)} className="cursor-pointer" size={20} />
-              <Trash
-                onClick={() => {
-                  console.log(task.id);
-                  setDeleteId(task.id);
-                }}
-                className="cursor-pointer"
-                size={20}
-              />
-            </TableCell>
-          </TableRow>
+          <TaskTableRow key={task.id} task={task} index={index} />
         ))}
       </TableBody>
       <EditTaskDialog />
---
+++ components/TaskTableRow.tsx
@@ -0,0 +1,107 @@
+import React, { Fragment, useState } from "react";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { Task } from "@/types/task";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import StatusBadge from "./StatusBadge";
+import { Priority } from "@/types/task";
+import { Edit, Trash } from "lucide-react";
+import { useTasks } from "@/context/TaskContext";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
+
+const TaskTableRow = ({ task, index }: { task: Task; index: number }) => {
+  const { setEdit, setDeleteId, updateTask } = useTasks();
+  const [mobileTableData, setMobileTableData] = useState(false);
+  return (
+    <Fragment key={task.id}>
+      <TableRow className="hidden md:table-row">
+        <TableCell className="whitespace-nowrap">{index + 1}</TableCell>
+        <TableCell className="truncate">{task.title}</TableCell>
+        <TableCell className="truncate">{task.description}</TableCell>
+        <TableCell className="whitespace-nowrap">{task.dueDate}</TableCell>
+        <TableCell className="whitespace-nowrap">
+          <StatusBadge status={task.status} />
+        </TableCell>
+        <TableCell className="whitespace-nowrap">
+          <Select value={task.priority} onValueChange={(e: Priority) => updateTask({ ...task, priority: e })}>
+            <SelectTrigger className="border-black w-[110px]">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="High">High</SelectItem>
+              <SelectItem value="Medium">Medium</SelectItem>
+              <SelectItem value="Low">Low</SelectItem>
+            </SelectContent>
+          </Select>
+        </TableCell>
+        <TableCell className="flex justify-center gap-2 mt-2">
+          <Edit onClick={() => setEdit(task)} className="cursor-pointer" size={20} />
+          <Trash
+            onClick={() => {
+              console.log(task.id);
+              setDeleteId(task.id);
+            }}
+            className="cursor-pointer"
+            size={20}
+          />
+        </TableCell>
+      </TableRow>
+      <TableRow className={`md:hidden ${index % 2 !== 0 && "bg-[#FFF9F8]"}`}>
+        <TableCell className="font-semibold text-primary w-32">Sr.No</TableCell>
+        <TableCell className="flex justify-between">
+          {index + 1}
+          {mobileTableData ? <FaChevronUp onClick={() => setMobileTableData(!mobileTableData)} /> : <FaChevronDown onClick={() => setMobileTableData(!mobileTableData)} />}
+        </TableCell>
+      </TableRow>
+      <TableRow className={`md:hidden ${index % 2 !== 0 && "bg-[#FFF9F8]"}`}>
+        <TableCell className="font-semibold text-primary w-32">Title</TableCell>
+        <TableCell>{task.title}</TableCell>
+      </TableRow>
+      {mobileTableData && (
+        <>
+          <TableRow className="md:hidden">
+            <TableCell className="font-semibold text-primary">Description</TableCell>
+            <TableCell className="w-64 break-words whitespace-pre-wrap">{task.description}</TableCell>
+          </TableRow>
+          <TableRow className="md:hidden">
+            <TableCell className="font-semibold text-primary">Due Date</TableCell>
+            <TableCell>{task.dueDate}</TableCell>
+          </TableRow>
+          <TableRow className="md:hidden">
+            <TableCell className="font-semibold text-primary">Status</TableCell>
+            <TableCell>
+              <StatusBadge status={task.status} />
+            </TableCell>
+          </TableRow>
+          <TableRow className="md:hidden">
+            <TableCell className="font-semibold text-primary">Priority</TableCell>
+            <TableCell className="flex justify-between items-center">
+              <Select value={task.priority} onValueChange={(e: Priority) => updateTask({ ...task, priority: e })}>
+                <SelectTrigger className="border-black w-[110px]">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="High">High</SelectItem>
+                  <SelectItem value="Medium">Medium</SelectItem>
+                  <SelectItem value="Low">Low</SelectItem>
+                </SelectContent>
+              </Select>
+              <div className="flex gap-4">
+                <Edit onClick={() => setEdit(task)} className="cursor-pointer" size={20} />
+                <Trash
+                  onClick={() => {
+                    console.log(task.id);
+                    setDeleteId(task.id);
+                  }}
+                  className="cursor-pointer"
+                  size={20}
+                />
+              </div>
+            </TableCell>
+          </TableRow>
+        </>
+      )}
+    </Fragment>
+  );
+};
+
+export default TaskTableRow;
