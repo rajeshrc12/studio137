890d2b1cce8e47a35e11a62f50dc6915adafb229
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-04-26 20:09:10 +0530

    img fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <div className="flex flex-col gap-5 w-full px-10 py-5 bg-[#FCFCFC]">
         <div className="flex justify-between items-center">
           <div>
-            <Image height={50} width={50} alt="logo" src={"/logo.jpg"} />
+            <Image unoptimized height={50} width={50} alt="logo" src={"/logo.jpg"} />
             <h1 className="font-bold text-xl mt-5">Tasks</h1>
           </div>
           <Filter />
